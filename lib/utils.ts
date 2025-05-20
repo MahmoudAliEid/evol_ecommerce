39b07a449436359b1062322166f85b92d9c1d241
mahmoudalieid <alim46792@gemail.com>
2025-05-20 14:29:44 +0300

    Refactor code structure for improved readability and maintainability

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatNumberWithDecimal = (num: number): string => {
+  const [integerPart, decimalPart] = num.toString().split(".");
+  const formattedIntegerPart = integerPart.replace(
+    /\B(?=(\d{3})+(?!\d))/g,
+    ","
+  );
+  return decimalPart
+    ? `${formattedIntegerPart}.${decimalPart.padEnd(2, "0")}`
+    : formattedIntegerPart;
+};
+
+export const toSlug = (text: string): string => {
+  return text
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+|-+$/g, "");
+};
---
+++ lib/vaildator.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import { formatNumberWithDecimal } from "./utils";
+
+const Price = (field: string) => {
+  return z.coerce
+    .number()
+    .refine(
+      (value) => /^\d+(\.\d{1,2})?$/.test(formatNumberWithDecimal(value)),
+      `${field} must be a valid price with up to two decimal places (e.g., 10.00)`
+    );
+};
+
+export const ProductInputSchema = z.object({
+  name: z.string().min(3, "Name must be least 3 characters long"),
+  slug: z.string().min(3, "Slug must be least 3 characters long"),
+  category: z.string().min(1, "category is required"),
+  images: z.array(z.string()).min(1, "Product must have at least one image"),
+  brand: z.string().min(1, "Brand is required"),
+  description: z.string().min(1, "Description is required"),
+  isPublished: z.boolean().optional(),
+  price: Price("Price"),
+  listPrice: Price("List Price"),
+  countInStock: z.coerce
+    .number()
+    .int()
+    .nonnegative("Count in stock must be a non-negative integer"),
+  tags: z.array(z.string()).default([]),
+  sizes: z.array(z.string()).default([]),
+  colors: z.array(z.string()).default([]),
+  avgRating: z.coerce
+    .number()
+    .min(0, "Avg rating must be at least 0")
+    .max(5, "Avg rating must be at most 5"),
+  numReviews: z.coerce
+    .number()
+    .int()
+    .nonnegative("Number of reviews must be a non-negative integer"),
+  reviews: z.array(z.string()).default([]),
+  numSales: z.coerce
+    .number()
+    .int()
+    .nonnegative("Number of sales must be a non-negative integer"),
+});
